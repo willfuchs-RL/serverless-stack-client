d74e47d3f0c51c3b434a29a0fe3886ff4f9e05a1
William Fuchs <william.fuchs@WillFuchs-MBPi.local>
2020-06-24 11:00:16 -0700

    set up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "will-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://4b9f5gylf2.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_OeIi8z28B",
+      APP_CLIENT_ID: "660vgqevsso10hg98u5f650g0m",
+      IDENTITY_POOL_ID: "us-east-2:aa9d1bba-b619-4d28-a4e6-33058a361122"
+    }
+  };
